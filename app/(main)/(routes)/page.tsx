e31d8df7365930b067e230ac35fc621db7156c17
sealproton <sealproton@gmail.com>
2023-10-12 15:13:56 +0700

    feat: add background Mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import { UserButton } from '@clerk/nextjs';
+import { ModeToggle } from '@/components/mode-toggle';
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl='/' />
+      <ModeToggle />
       <p>Hello discord clone</p>
     </div>
   );
